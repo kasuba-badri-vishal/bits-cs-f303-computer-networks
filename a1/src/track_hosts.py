126638edb7204aac17f232cef83c2ad6e575a5c9
Kasuba Badri Vishal <kasubabadri@gmail.com>
2020-02-08 10:18:10 +0530

    Replaced files

--- a1/src/track_hosts.py
+++ a1/src/track_hosts.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 import argparse
 import time
 import numpy as np
-# from datetime import datetime as dt
 
 parser = argparse.ArgumentParser(description="Tracking hosts.")
 parser.add_argument('-f', type=int, default = 4, \
@@ -15,8 +14,7 @@ parser.add_argument('-s', type=int, default=24, help="subnet mask")
 def plot_graph():
     x=dict()
     y=[]
-
-    with open('output2.csv', 'r') as csvfile:
+    with open('output.csv', 'r') as csvfile:
         plots= csv.reader(csvfile, delimiter=',')
         for row in plots:
             k = row[1].split()[0]
@@ -34,7 +32,6 @@ def plot_graph():
     plt.ylabel('Number of Hosts online')
     plt.xlabel('Time (hrs)')
     ax.legend()
-    # plt.xlim(0,24.1)
     plt.xticks(np.arange(0, 24, 1.0))
     plt.show()
 
@@ -47,26 +44,25 @@ def write_output(data):
         csvwriter.writerow(data)
 
 def find_hosts(subnet):
+
     host = f'172.16.34.0/{subnet}'
     result = subprocess.check_output(['nmap','-sP',host]).decode('utf-8')
     print(result)
-    # match = re.search(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2} IST', result)
     cur_time = time.localtime()
     match = f'{cur_time.tm_year}-{cur_time.tm_mon}-{cur_time.tm_mday} {cur_time.tm_hour}:{cur_time.tm_min} IST'
     match2 = re.search(r'[0-9]+ hosts up', result)
     res = []
     res.append(match2.group().split()[0])
-    # res.append(match.group())
     res.append(match)
     res.append(subnet)
     return res
 
 
 if __name__ == "__main__":
-    # args = parser.parse_args()
-    # while(True):
-    #     data = find_hosts(args.s)
-    #     print(data)
-    #     write_output(data)
-    #     time.sleep((3600/args.f))
+    args = parser.parse_args()
+    while(True):
+        data = find_hosts(args.s)
+        print(data)
+        write_output(data)
+        time.sleep((3600/args.f))
     plot_graph()
